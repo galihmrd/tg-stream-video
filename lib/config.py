3a6fb21cea68ba3d5da17aa3b02e7600d2607637
galihmrd <galihmardiana15@gmail.com>
2023-06-13 02:13:59 +0000

    Update pyrogram and pytgcalls version

--- lib/config.py
+++ lib/config.py
@@ -1,8 +1,6 @@
-import os
-
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-SESSION_NAME = os.getenv("SESSION_NAME")
-USERNAME_BOT = os.getenv("USERNAME_BOT")
-COMMAND_PREFIXES = os.getenv("COMMAND_PREFIXES", "!")
+API_ID = 
+API_HASH = 
+BOT_TOKEN = 
+SESSION_NAME = 
+USERNAME_BOT = 
+COMMAND_PREFIXES = 
